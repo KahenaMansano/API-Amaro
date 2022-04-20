cd2cd114c368f0d113ef2efd7c1ae384bd560c3c
Kahena Mansano <kahenamansanoalmeida@gmail.com>
2022-04-19 21:38:53 -0400

    add Procfile e movendo knex

--- src/knex.ts
+++ src/knex.ts
@@ -1,4 +1,4 @@
 const environment = process.env.ENVIRONMENT || 'development'
-const config = require('../knexfile')[environment]
+const config = require('./knexfile')[environment]
 
 export default require('knex')(config)
--- src/knexfile.ts
+++ src/knexfile.ts
@@ -32,18 +32,13 @@ const config: { [key: string]: Knex.Config } = {
   },
 
   production: {
-    client: 'postgresql',
+    client: 'mysql',
     connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+      host: process.env.DB_HOST,
+      port: 3306,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME
     }
   }
 }
